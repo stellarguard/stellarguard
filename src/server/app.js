74dad34258bc3d99b7a622e3bba214867efd24cd
pselden <pselden4@gmail.com>
2018-02-05 23:37:02 -0500

    Fix build command.

--- src/server/app.js
+++ src/server/app.js
@@ -21,7 +21,7 @@ app.use(logger('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser('keyboard cat'));
+app.use(cookieParser(config.sessionSecret));
 
 session.configure(app);
 app.use('/api', apiRoutes);
