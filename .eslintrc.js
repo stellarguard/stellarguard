c415ad420f75215c87ba4497d94a183b4d43fed3
pselden <pselden4@gmail.com>
2018-02-08 00:37:25 -0500

    Lint changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,8 @@ module.exports = {
   extends: ['eslint:recommended', 'plugin:react/recommended'],
   parser: 'babel-eslint',
   rules: {
-    'react/prop-types': false
-  }
+    'react/prop-types': false,
+    'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }]
+  },
+  env: { browser: true, node: true }
 };
