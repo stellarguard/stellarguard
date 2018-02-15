c69475876a57380b40fe74e1c50935000e39557f
pselden <pselden4@gmail.com>
2018-02-15 09:55:35 -0500

    Add shake keyframe animation to form field helper text.

--- src/ui/components/FormFieldHelperText.jsx
+++ src/ui/components/FormFieldHelperText.jsx
@@ -2,14 +2,42 @@ import React, { Component } from 'react';
 import { withStyles } from 'material-ui';
 import { observer } from 'mobx-react';
 
-const styles = theme => ({});
+const styles = theme => ({
+  hasError: {
+    animation: `shake ${theme.transitions.duration.shortest}ms ${
+      theme.transitions.easing.easeInOut
+    } both`,
+    display: 'inline-block'
+  },
+  '@keyframes shake': {
+    '10%, 90%': {
+      transform: `translate3d(-1px, 0, 0)`
+    },
+
+    '20%, 80%': {
+      transform: `translate3d(-2px, 0, 0)`
+    },
+
+    '30%, 50%, 70%': {
+      transform: `translate3d(-4px, 0, 0)`
+    },
+
+    '40%, 60%': {
+      transform: `translate3d(4px, 0, 0)`
+    }
+  }
+});
 
 @observer
 @withStyles(styles)
 class FormFieldHelperText extends Component {
   render() {
     const { classes, children, error, touched } = this.props;
-    return error && touched ? error : children;
+    return error && touched ? (
+      <span className={classes.hasError}>{error}</span>
+    ) : (
+      children
+    );
   }
 }
 
