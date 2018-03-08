2105ea905664a70f6ba909f7bc378414423a4c68
pselden <pselden4@gmail.com>
2018-03-08 02:34:33 -0500

    Minor code cleanup.

--- src/server/lib/users/userValidator.js
+++ src/server/lib/users/userValidator.js
@@ -1,6 +1,5 @@
 const passwordValidator = require('./passwordValidator');
 const users = require('validators/users');
-const { CommonPasswordError } = require('errors/user');
 
 exports.validate = async function(user) {
   await users.validate(user);
