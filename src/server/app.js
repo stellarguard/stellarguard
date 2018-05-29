adc070b1ebd6b32f4e220b56be6122eb9b32454d
pselden <pselden4@gmail.com>
2018-05-29 11:31:12 -0400

    Fix two-factor auth data URIs by allowing data: attributes in img CSP

--- src/server/app.js
+++ src/server/app.js
@@ -69,7 +69,7 @@ app.use(
         'fonts.gstatic.com'
       ],
       'base-uri': [csp.SELF],
-      'img-src': ['*'],
+      'img-src': ['*', 'data:'],
       'block-all-mixed-content': true
     }
   })
