2d6b839096704dc70aa03a97a1afad9aa1145681
pselden <pselden4@gmail.com>
2018-06-02 02:31:58 -0400

    Encrypt all unencrypted secret keys.

---
+++ migrations/20180602055429-encrypt-secret-keys.js
@@ -0,0 +1,43 @@
+'use strict';
+
+var dbm;
+var type;
+var seed;
+
+var { crypto } = require('../src/server/lib/utils');
+
+/**
+ * We receive the dbmigrate dependency from dbmigrate initially.
+ * This enables us to not have to rely on NODE_PATH.
+ */
+exports.setup = function(options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+exports.up = async function(db) {
+  const rows = await db.all(
+    `SELECT * from "user" WHERE encrypted_signer_secret_key IS NULL`
+  );
+
+  for (const row of rows) {
+    const encryptedSignerSecretKey = await crypto.encrypt(
+      row.signer_secret_key
+    );
+
+    await db.runSql(
+      `UPDATE "user"
+       SET encrypted_signer_secret_key = ?
+       WHERE id = ?
+      `,
+      [encryptedSignerSecretKey, row.id]
+    );
+  }
+};
+
+exports.down = async function(db) {};
+
+exports._meta = {
+  version: 1
+};
