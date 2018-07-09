67926447d335fe8fc920c1348a2557c37d89e563
pselden <pselden4@gmail.com>
2018-07-09 11:26:19 -0400

    Replace speakasy with otplib, which is better maintained
    Has several fixes including improved randomness when generating secrets and theoretically better support for Google Authenticator for Android.

--- src/server/lib/tfa/otp.js
+++ src/server/lib/tfa/otp.js
@@ -1,16 +1,10 @@
-const speakeasy = require('speakeasy');
+const otplib = require('otplib');
 const qrcode = require('qrcode');
 
 const ISSUER = 'StellarGuard';
 
 async function generateSecret(email) {
-  const speakeasySecret = speakeasy.generateSecret({
-    name: email,
-    issuer: ISSUER,
-    length: 16
-  });
-
-  const secret = speakeasySecret.base32;
+  const secret = otplib.authenticator.generateSecret();
   const qrCode = await generateQrCode(email, secret);
   return {
     secret,
@@ -23,23 +17,12 @@ async function generateQrCode(email, secret) {
   return await qrcode.toDataURL(otpAuthUrl);
 }
 
-function getOtpAuthUrl(label, secret) {
-  const otpAuthUrlOptions = {
-    label: `${encodeURIComponent(ISSUER)}:${encodeURIComponent(label)}`,
-    secret: secret,
-    encoding: 'base32',
-    ISSUER
-  };
-
-  return speakeasy.otpauthURL(otpAuthUrlOptions);
+function getOtpAuthUrl(email, secret) {
+  return otplib.authenticator.keyuri(email, ISSUER, secret);
 }
 
 function verifyToken(token, secret) {
-  return speakeasy.totp.verify({
-    secret,
-    token,
-    encoding: 'base32'
-  });
+  return otplib.authenticator.check(token, secret);
 }
 
 module.exports = {
